fd681a094ac9ccdba66fb656d5d1aea8fab65f1a
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-01-28 17:58:31 +0100

    Tags now with colors

--- application/settings.py
+++ application/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'nalch_application',
     'modeltranslation',
+    'colorful',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- nalch_application/models.py
+++ nalch_application/models.py
@@ -1,9 +1,12 @@
+from colorful.fields import RGBColorField
+
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
+    color = RGBColorField()
 
     def __unicode__(self):
         return self.name
