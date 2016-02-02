891e5e17c4dbae4d9f1c032983f668b5b9f3a0f7
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-02-02 21:56:24 +0100

    no prefix needed

--- application/urls.py
+++ application/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^application/', include('nalch_application.urls')),
+    url(r'^/', include('nalch_application.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
 )
