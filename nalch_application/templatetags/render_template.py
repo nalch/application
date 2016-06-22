345340ed3358b01432ce8e47e25764c81ce20b25
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-06-22 12:08:29 +0200

    add documentation, resolves #5

--- nalch_application/templatetags/render_template.py
+++ nalch_application/templatetags/render_template.py
@@ -10,6 +10,15 @@ register = template.Library()
 
 @register.simple_tag
 def render_template(text, **kwargs):
+    """
+    render a template string with a context derived from the keyword arguments
+    :param text: the string to be rendered
+    :type text: str
+    :param kwargs: the keywordarguments, that are keys and values of the generated context
+    :type kwargs: dict
+    :return: the rendered template
+    :rtype str
+    """
     texttemplate = Template(text)
     context = Context(kwargs)
     return texttemplate.render(context)
