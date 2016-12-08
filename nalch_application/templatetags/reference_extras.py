0f779292fad69a2a614e56a1e2b7bcade0045a42
Kristian Scholze <Scholze.Kristian@gmail.com>
2016-12-08 11:14:32 +0100

    sort tags by displayname, add doc

--- nalch_application/templatetags/reference_extras.py
+++ nalch_application/templatetags/reference_extras.py
@@ -1,3 +1,5 @@
+import functools
+
 from django import template
 from django.core.exceptions import ObjectDoesNotExist
 from django.template.loader import render_to_string
@@ -14,6 +16,15 @@ register = template.Library()
 
 @register.simple_tag
 def reference_table(applicant, references):
+    """
+    render a short table with information of reference projects
+    :param applicant: the applicant to get the references for
+    :type applicant: Applicant
+    :param references: all references
+    :type references: lst
+    :return: rendered template
+    :rtype: str
+    """
     projects = Project.objects.filter(publish=True, user__user__username=applicant)
     tags = WeightedTag.objects.filter(project__in=projects)
     context_dict = {
@@ -26,6 +37,15 @@ def reference_table(applicant, references):
 
 @register.simple_tag
 def knowledge_level(tag, project):
+    """
+    return an icon denoting a WeightedTag's knowledgelevel
+    :param tag: the tag to get the knowledgelevel for
+    :type tag: WeightedTag
+    :param project: the project for the tag (knowledgelevels/usage is different for the same tag on different projects)
+    :type project: Project
+    :return: the icon for the tag's knowledgelevel
+    :rtype: str
+    """
     try:
         return WeightedTag.objects.get(project=project, tag=tag).weight.icon
     except ObjectDoesNotExist:
@@ -34,6 +54,13 @@ def knowledge_level(tag, project):
 
 @register.simple_tag
 def reference(reference_item, group=None):
+    """
+    render a detailled reference
+    :param reference_item: the project to render
+    :type reference_item: Project
+    :return: rendered template as string
+    :rtype: str
+    """
     context_dict = {
         'group': group,
         'reference': reference_item,
@@ -43,6 +70,13 @@ def reference(reference_item, group=None):
 
 @register.simple_tag
 def small_reference(reference_item):
+    """
+    render a reference's overview
+    :param reference_item: the project to render
+    :type reference_item: Project
+    :return: rendered template as string
+    :rtype: str
+    """
     context_dict = {
         'reference': reference_item,
     }
@@ -72,3 +106,24 @@ def icon_css(iconmixin):
             return icon['fontawesome_css']
         return iconmixin.icon
     return iconmixin
+
+
+@register.filter
+def sort(lst, key_name):
+    """
+    sort a list after a freely chosen key. Similar to the dictsort templatetag but adding recursive keys.
+    f.e. myset.all|sort:"nested.keys"
+    :param lst: the list to be sorted
+    :type lst: lst
+    :param key_name: string denoting the sort key
+    :type key_name: str
+    :return: sorted list
+    :rtype: list
+    """
+    return sorted(
+            lst,
+            key=lambda item: functools.reduce(
+                    lambda obj, key: getattr(obj, key),
+                    [item] + key_name.split('.')
+            )
+    )
